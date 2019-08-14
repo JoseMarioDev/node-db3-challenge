1cf2c31dea7f7814969df4cc250327861cc3d258
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-08-14 14:42:00 -0500

    find, findById created and working

---
+++ schemes/scheme-model.js
@@ -0,0 +1,40 @@
+const db = require('../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  // findSteps,
+  // add,
+  // update,
+  // remove,
+};
+
+function find() {
+  return db('schemes');
+}
+function findById(id) {
+  return db('schemes').where({ id });
+}
+
+// function add(user) {
+//   return db('users').insert(user);
+// }
+
+// function getUserPosts(id) {
+//   return db('posts as p')
+//     .innerJoin('users as u', 'p.user_id', '=', 'u.id')
+//     .select('p.id', 'p.contents', 'u.username as postedBy')
+//     .where({ user_id: id });
+// }
+
+// function update(id, changes) {
+//   return db('users')
+//     .where({ id })
+//     .update(changes);
+// }
+
+// function remove(id) {
+//   return db('users')
+//     .where({ id })
+//     .del();
+// }
